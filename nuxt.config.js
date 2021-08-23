b2d4fcda161d273682409691811d07d336ead222
YoungMayor <youngmayor08@gmail.com>
2021-08-23 13:30:38 +0100

    Fixed design errors

--- nuxt.config.js
+++ nuxt.config.js
@@ -30,7 +30,8 @@ export default {
 
     // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
     plugins: [
-        '@/plugins/balm-ui'
+        '@/plugins/balm-ui',
+        '@/plugins/fullpage',
     ],
 
     // Auto import components: https://go.nuxtjs.dev/config-components
--- pages/index.vue
+++ pages/index.vue
@@ -1,35 +1,6 @@
 <template>
     <div>
-        <!-- <ul id="menu">
-            <li data-menuanchor="welcome">
-                <a href="#anchor1">Welcome to iJokes</a>
-            </li>
-            <li data-menuanchor="selectCategory">
-                <a href="#anchor2">Select jokes category</a>
-            </li>
-            <li data-menuanchor="jokes">
-                <a href="#anchor3">LOL</a>
-            </li>
-            <li data-menuanchor="footer">
-                <a href="#anchor3">Footer</a>
-            </li>
-        </ul> -->
-        <!-- <ui-tab-bar id="menu" av-model="active">
-            <ui-tab
-                v-for="(tab, index) in {
-                    welcome: 'Welcome to iJokes',
-                    selectCategory: 'Select jokes category',
-                    jokes: 'LOL',
-                    footer: 'Footer',
-                }"
-                :key="index"
-                :data-menuanchor="index"
-            >
-                {{ tab }}
-            </ui-tab>
-        </ui-tab-bar> -->
-
-        <div id="fullpage">
+        <full-page ref="fullpage" :options="fullPageConfig" id="fullpage">
             <div class="section" id="section-welcome">
                 <div class="text-center">
                     <h1 class="app-title ma-0">
@@ -38,60 +9,6 @@
                     </h1>
                     <p class="app-catchphrase">For the love of laughs</p>
                 </div>
-
-                <!-- <ui-card
-                    class="intro-card"
-                    outlined
-                    :style="{
-                        borderColor: 'transparent',
-                    }"
-                >
-                    <ui-card-content class="intro-card__content">
-                        <ui-card-media
-                            square
-                            class="intro-card__media"
-                            xxxstyle="{
-                                backgroundImage: `url('/mask-clear.png')`,
-                            }"
-                        >
-                            <ui-card-media-content
-                                class="intro-card__media-content"
-                            >
-                                <div
-                                    class="
-                                        text-center
-                                        intro-card__media-content-wrapper
-                                    "
-                                >
-                                    <div>
-                                        <h2 :class="[$tt('headline2')]">
-                                            iJokes
-                                        </h2>
-
-                                        <div>For the love of laughs...</div>
-                                    </div>
-
-                                    <ui-chips
-                                        id="input-chip-set"
-                                        type="action"
-                                        :style="{
-                                            display: 'flex',
-                                            justifyContent: 'center',
-                                        }"
-                                    >
-                                        <ui-chip
-                                            v-for="(cat, index) in categories"
-                                            :key="index"
-                                            icon="face"
-                                        >
-                                            {{ String(cat).toUpperCase() }}
-                                        </ui-chip>
-                                    </ui-chips>
-                                </div>
-                            </ui-card-media-content>
-                        </ui-card-media>
-                    </ui-card-content>
-                </ui-card> -->
             </div>
 
             <div class="section" id="section-select-category">
@@ -111,6 +28,7 @@
                         v-for="(cat, index) in categories"
                         :key="index"
                         icon="face"
+                        @click="changeCategory(cat)"
                     >
                         {{ String(cat).toUpperCase() }}
                     </ui-chip>
@@ -118,29 +36,209 @@
             </div>
 
             <div class="section" id="section-jokes">
-                <div class="slide" id="slide1" v-for="i in 36" :key="i">
-                    <JokeCard v-if="i % 2" :payload="payload.single" />
-                    <JokeCard v-else :payload="payload.twopart" />
+                <div
+                    :style="{
+                        position: 'absolute',
+                        bottom: '38px',
+                        left: '0',
+                        right: '0',
+                        height: '77px',
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                    }"
+                >
+                    <ui-button
+                        icon="auto_stories"
+                        @click="getItems(10)"
+                        :style="{
+                            zIndex: 5,
+                        }"
+                        :disabled="loading"
+                        class="mdc-theme--text-primary-on-dark"
+                    >
+                        <span>
+                            {{
+                                loading
+                                    ? "Please wait..."
+                                    : `More ${selectedCategory} jokes...`
+                            }}
+                        </span>
+                    </ui-button>
+                </div>
+
+                <div
+                    v-if="!selectedCategory"
+                    :style="{
+                        position: 'absolute',
+                        top: '0',
+                        bottom: '0',
+                        left: '0',
+                        right: '0',
+                        zIndex: 8,
+                        backgroundColor: '#1abc9ba6',
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                    }"
+                >
+                    <span :class="[$tt('headline2'), 'text-center']">
+                        No category selected
+                    </span>
+                </div>
+
+                <div
+                    class="slide"
+                    xxxid="`joke-slide-${i}`"
+                    v-for="joke in jokes"
+                    :key="joke.id"
+                >
+                    <JokeCard :payload="joke" />
+                    <!-- <JokeCard v-if="i % 2" :payload="payload.single" />
+                    <JokeCard v-else :payload="payload.twopart" /> -->
                 </div>
             </div>
 
             <div class="section fp-auto-height" id="section-footer">
-                <div class="intro">
-                    <h1>Hope you enjoyed it</h1>
+                <div
+                    class="app-footer"
+                    :style="{
+                        padding: '20px',
+                        color: 'white !important',
+                        fontSize: '15px',
+                        textAlign: 'center',
+                    }"
+                >
+                    <span>
+                        &copy; 2021
+                        <a href="https://youngmayor.dev">Meyoron Aghogho</a>
+                        All rights reserved.
+                    </span>
                 </div>
             </div>
-        </div>
+        </full-page>
     </div>
 </template>
 
 <script>
-import fullpage from "fullpage.js";
-
 export default {
     name: "IndexPage",
 
     data() {
         return {
+            fullPageConfig: {
+                //Navigation
+                menu: "#menu",
+                lockAnchors: false,
+                anchors: ["welcome", "selectCategory", "jokes", "footer"],
+                navigation: true,
+                navigationPosition: "right",
+                navigationTooltips: [
+                    "Welcome to iJokes",
+                    "Select jokes category",
+                    "Jokes",
+                    "Footer",
+                ],
+                showActiveTooltip: false,
+                slidesNavigation: true,
+                slidesNavPosition: "top",
+
+                //Scrolling
+                css3: true,
+                scrollingSpeed: 700,
+                autoScrolling: true,
+                fitToSection: true,
+                fitToSectionDelay: 1000,
+                scrollBar: true,
+                easing: "easeInOutCubic",
+                easingcss3: "ease",
+                loopBottom: false,
+                loopTop: false,
+                loopHorizontal: true,
+                continuousVertical: false,
+                continuousHorizontal: false,
+                scrollHorizontally: false,
+                interlockedSlides: false,
+                dragAndMove: false,
+                offsetSections: false,
+                resetSliders: false,
+                fadingEffect: false,
+                normalScrollElements: "#element1, .element2",
+                scrollOverflow: false,
+                scrollOverflowReset: false,
+                scrollOverflowOptions: null,
+                touchSensitivity: 15,
+                bigSectionsDestination: null,
+
+                //Accessibility
+                keyboardScrolling: true,
+                animateAnchor: true,
+                recordHistory: true,
+
+                //Design
+                controlArrows: true,
+                verticalCentered: true,
+                sectionsColor: ["#C63D0F", "#99e49f", "#1BBC9B", "#7E8F7C"],
+                paddingTop: "3em",
+                paddingBottom: "10px",
+                fixedElements: "#header, .footer",
+                responsiveWidth: 0,
+                responsiveHeight: 0,
+                responsiveSlides: false,
+                parallax: false,
+                parallaxOptions: {
+                    type: "reveal",
+                    percentage: 62,
+                    property: "translate",
+                },
+                dropEffect: false,
+                dropEffectOptions: {
+                    speed: 2300,
+                    color: "#F82F4D",
+                    zIndex: 9999,
+                },
+                waterEffect: false,
+                waterEffectOptions: {
+                    animateContent: true,
+                    animateOnMouseMove: true,
+                },
+                cards: false,
+                cardsOptions: {
+                    perspective: 100,
+                    fadeContent: true,
+                    fadeBackground: true,
+                },
+
+                //Custom selectors
+                sectionSelector: ".section",
+                slideSelector: ".slide",
+
+                lazyLoading: true,
+
+                //events
+                onLeave: function (origin, destination, direction) {},
+                afterLoad: function (origin, destination, direction) {},
+                afterRender: function () {},
+                afterResize: function (width, height) {},
+                afterReBuild: function () {},
+                afterResponsive: function (isResponsive) {},
+                afterSlideLoad: function (
+                    section,
+                    origin,
+                    destination,
+                    direction
+                ) {},
+                onSlideLeave: function (
+                    section,
+                    origin,
+                    destination,
+                    direction
+                ) {},
+
+                // license
+                licenseKey: "YOUR_KEY_HERE",
+            },
+
             categories: [
                 "programming",
                 "political",
@@ -148,6 +246,10 @@ export default {
                 "racist",
                 "sexist",
             ],
+
+            jokes: [],
+            selectedCategory: null,
+            loading: false,
 
             payload: {
                 twopart: {
@@ -182,113 +284,45 @@ export default {
     },
 
     methods: {
-        //
+        changeCategory(cat) {
+            this.selectedCategory = cat;
+
+            this.jokes = [];
+            this.$refs.fullpage.api.moveTo("jokes");
+            this.getItems();
+        },
+
+        getItems(length = 10) {
+            this.loading = true;
+
+            setTimeout(() => {
+                for (let index = 0; index < length; index++) {
+                    this.jokes.push({
+                        category: "Programming",
+                        type: "twopart",
+                        setup: "Example Buildup: Jokes about anti-vaxxer parents never get's old",
+                        delivery: "Example Delivery: Just like their kids",
+                        flags: {
+                            nsfw: true,
+                            religious: true,
+                            political: false,
+                        },
+                        id: Math.random() * index + index,
+                        safe: true,
+                        lang: "en",
+                    });
+                }
+
+                setTimeout(() => {
+                    this.$refs.fullpage.build();
+                    this.loading = false;
+                }, 1000);
+            }, 1500);
+        },
     },
 
     mounted() {
-        var myFullpage = new fullpage("#fullpage", {
-            //Navigation
-            menu: "#menu",
-            lockAnchors: false,
-            anchors: ["welcome", "selectCategory", "jokes", "footer"],
-            navigation: true,
-            navigationPosition: "right",
-            navigationTooltips: [
-                "Welcome to iJokes",
-                "Select jokes category",
-                "Jokes",
-                "Footer",
-            ],
-            showActiveTooltip: false,
-            slidesNavigation: true,
-            slidesNavPosition: "bottom",
-
-            //Scrolling
-            css3: true,
-            scrollingSpeed: 700,
-            autoScrolling: true,
-            fitToSection: true,
-            fitToSectionDelay: 1000,
-            scrollBar: true,
-            easing: "easeInOutCubic",
-            easingcss3: "ease",
-            loopBottom: false,
-            loopTop: false,
-            loopHorizontal: true,
-            continuousVertical: false,
-            continuousHorizontal: false,
-            scrollHorizontally: false,
-            interlockedSlides: false,
-            dragAndMove: false,
-            offsetSections: false,
-            resetSliders: false,
-            fadingEffect: false,
-            normalScrollElements: "#element1, .element2",
-            scrollOverflow: false,
-            scrollOverflowReset: false,
-            scrollOverflowOptions: null,
-            touchSensitivity: 15,
-            bigSectionsDestination: null,
-
-            //Accessibility
-            keyboardScrolling: true,
-            animateAnchor: true,
-            recordHistory: true,
-
-            //Design
-            controlArrows: true,
-            verticalCentered: true,
-            sectionsColor: ["#C63D0F", "#99e49f", "#1BBC9B", "#7E8F7C"],
-            paddingTop: "3em",
-            paddingBottom: "10px",
-            fixedElements: "#header, .footer",
-            responsiveWidth: 0,
-            responsiveHeight: 0,
-            responsiveSlides: false,
-            parallax: false,
-            parallaxOptions: {
-                type: "reveal",
-                percentage: 62,
-                property: "translate",
-            },
-            dropEffect: false,
-            dropEffectOptions: { speed: 2300, color: "#F82F4D", zIndex: 9999 },
-            waterEffect: false,
-            waterEffectOptions: {
-                animateContent: true,
-                animateOnMouseMove: true,
-            },
-            cards: false,
-            cardsOptions: {
-                perspective: 100,
-                fadeContent: true,
-                fadeBackground: true,
-            },
-
-            //Custom selectors
-            sectionSelector: ".section",
-            slideSelector: ".slide",
-
-            lazyLoading: true,
-
-            //events
-            onLeave: function (origin, destination, direction) {},
-            afterLoad: function (origin, destination, direction) {},
-            afterRender: function () {},
-            afterResize: function (width, height) {},
-            afterReBuild: function () {},
-            afterResponsive: function (isResponsive) {},
-            afterSlideLoad: function (
-                section,
-                origin,
-                destination,
-                direction
-            ) {},
-            onSlideLeave: function (section, origin, destination, direction) {},
-
-            // license
-            licenseKey: "YOUR_KEY_HERE",
-        });
+        //
     },
 };
 </script>
@@ -304,31 +338,6 @@ export default {
 
 #fullpage {
     color: white;
-
-    // .intro-card {
-    //     width: 90%;
-    //     margin: 5% auto;
-    //     max-width: 450px;
-    //     background-color: transparent;
-
-    //     .intro-card__media {
-    //         .intro-card__media-content {
-    //             display: flex;
-    //             flex-direction: column;
-    //             justify-content: flex-end;
-
-    //             .intro-card__media-content-wrapper {
-    //                 padding: 16px;
-    //                 background-image: linear-gradient(
-    //                     to bottom,
-    //                     rgba(0, 0, 0, 0) 0%,
-    //                     rgba(0, 0, 0, 0.5) 100%
-    //                 );
-    //                 color: white;
-    //             }
-    //         }
-    //     }
-    // }
 
     .app-title {
         font-size: 5em;
@@ -348,6 +357,12 @@ export default {
         .fp-controlArrow {
             bottom: 38px;
             top: unset;
+        }
+
+        .fp-slidesNav {
+            ul {
+                text-align: center;
+            }
         }
     }
 }
